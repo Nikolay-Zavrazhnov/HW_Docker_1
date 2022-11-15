43cb09dbd52e75bf33ab29a88000856284723a1b
Николай Завражнов <niko3avr@yandex.ru>
2022-11-15 18:27:25 +0400

    first commit

---
+++ 3.2-crud/README.md
@@ -0,0 +1,12 @@
+
+## 1. Building image
+
+```
+docker build ./DOCKER_HW/3.2-crud  --tag=stocks_products:0.1
+```
+
+## 2. Running the container
+
+```
+docker run --name=test_stocks_products -d -p 8000:8000 stocks_products:0.1
+```
---
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -0,0 +1,30 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer, StockProduct, ProductPositionSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['products']
+    #настройка для связанного поиска(допю задача)
+    search_fields = ['products__title', 'products__description']
+
+
+
+
+
